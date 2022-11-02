1a6b2c106e6c2f294895a27e2aeb46737d097033
bbalintb@stud.uni-obuda.hu <bbalintb@stud.uni-obuda.hu>
2022-11-02 10:20:44 +0100

    Add project files.

---
+++ Workshop05PTC_orai/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Workshop05PTC_orai.Models;
+
+namespace Workshop05PTC_orai.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<AppUser> Users{ get; set; }
+        public DbSet<Car>Cars{ get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Workshop05PTC_orai/Models/Car.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workshop05PTC_orai.Models
+{
+    public class Car {
+        [Key]
+        public string Id { get; set; }
+        public string Type { get; set; }
+
+        public int Price { get; set; }
+        public string? ImageFileName { get; set; }
+        public string? ContentType { get; set; }
+        public byte[]? Data { get; set; }
+
+        public Car()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
+
